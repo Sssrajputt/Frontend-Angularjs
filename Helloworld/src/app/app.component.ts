c10e3d6dc53d517086535a2b0dfa48cc0bd4adfb
Sssrajputt <149288600+Sssrajputt@users.noreply.github.com>
2025-03-07 11:50:14 +0530

    [Shubham Rajput] Added : UC2

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -3,12 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'] // Fixed typo: styleürls -> styleUrls
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Helloworld';
-
-  ngOnInit(): void {
-    this.title = "Hello from BridgeLabz."; // Initializes the title when the component is loaded
-  }
+  imgUrl = 'assets/BL_logo_square_jpg.jpg'; // Path to the BridgeLabz logo
 }
